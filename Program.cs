1ada60ac96c68faf69f0cbe676359b224675869a
janos1000 <birinyij@kkszki.hu>
2024-10-18 11:29:19 +0200

    Hőmérséklet bekérése is working.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Homerseklet
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        const int daysInWeek = 7;
+        double[] temperatures = new double[daysInWeek];
+        double sum = 0;
+
+        for (int i = 0; i < daysInWeek; i++)
         {
+            Console.Write($"Kérem, adja meg a(z) {i + 1}. napi hőmérsékletet (°C): ");
+            while (!double.TryParse(Console.ReadLine(), out temperatures[i]))
+            {
+                Console.WriteLine("Kérlek, érvényes hőmérsékletet adj meg!");
+            }
+            sum += temperatures[i];
+        }
+
+        double averageTemperature = sum / daysInWeek;
+
+        double maxTemperature = temperatures[0];
+        double minTemperature = temperatures[0];
+        bool hasFrostDay = false;
+
+        for (int i = 1; i < daysInWeek; i++)
+        {
+            if (temperatures[i] > maxTemperature)
+                maxTemperature = temperatures[i];
+
+            if (temperatures[i] < minTemperature)
+                minTemperature = temperatures[i];
+
+            if (temperatures[i] < 0)
+                hasFrostDay = true;
+        }
+
+
+        Console.WriteLine($"A hét átlaghőmérséklete: {averageTemperature:F2} °C");
+        Console.WriteLine($"A legmelegebb nap hőmérséklete: {maxTemperature} °C");
+        Console.WriteLine($"A leghidegebb nap hőmérséklete: {minTemperature} °C");
+
+
+        if (hasFrostDay)
+        {
+            Console.WriteLine("Fagypont alatti nap is volt!");
         }
     }
 }
